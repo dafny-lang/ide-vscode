462b35344b07fcbe77d9374f54781d1bcea774b3
just-me- <hess-marcel@bluewin.ch>
2020-03-16 20:43:35 +0100

    Server Exe Path lässt sich nun konfigurieren

--- src/server/dafnyLanguageClient.ts
+++ src/server/dafnyLanguageClient.ts
@@ -1,15 +1,22 @@
-import { workspace } from "vscode";
+import { workspace, WorkspaceConfiguration } from "vscode";
 import { LanguageClient, ServerOptions } from "vscode-languageclient";
 import { LanguageClientOptions } from "vscode-languageclient/lib/client";
 import { window } from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
 
+import { EnvironmentConfig } from "../stringRessources";
+
 export default class DafnyLanguageClient extends LanguageClient {
     constructor() {
-        // 2do: Production Folder Structure may be different. 
-        // Ask user for exe path if exe not found Ticket #45
-        const dafnyLangServerExe = path.join(__dirname, "../../../../dafny-language-server/Binaries/DafnyLanguageServer.exe");
+        const config: WorkspaceConfiguration = workspace.getConfiguration(EnvironmentConfig.Dafny);
+        var serverExePath : string | undefined = config.get("serverExePath");
+        if(serverExePath === undefined)  {
+            window.showErrorMessage("Server Executable not defined: please check your config for serverExePath");
+            return; 
+        }
+
+        const dafnyLangServerExe = path.join(__dirname, serverExePath);
 
         fs.exists(dafnyLangServerExe, (exist) => {
             if (!exist) {
