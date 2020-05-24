ef49e35f80cf81fe16219301495e7720915b20ea
Just Me <hess-marcel@bluewin.ch>
2020-05-25 01:31:50 +0200

    Neuer Zwischenstand Download

--- src/dafnyLanguageServerStartup/ILanguageServerInstaller.ts
+++ src/dafnyLanguageServerStartup/ILanguageServerInstaller.ts
@@ -3,12 +3,13 @@
  * This is an adapted interface version for the origin dafnyInstaller.ts:
  * https://github.com/DafnyVSCode/Dafny-VSCode/blob/develop/server/src/backend/dafnyInstaller.ts
  *
- * Once the new language server goes live, an installer based on the original code can be used.
+ * Once the new language server goes live, an installer based on the original code can be used. (for GitHub releases)
  * Until then, the current implementation "languageServerInstaller.ts" will be used.
- * It simulates the online availability of the Dafny language server on a private, separate server.
+ * It simulates the online availability of the Dafny language server on our GitLab CI server.
  * In our version, the upload of the server can only be automated in a limited way and is only a temporary interim solution.
  */
 export interface ILanguageServerInstaller {
+  anyVersionInstalled(): boolean;
   latestVersionInstalled(localVersion: string): Promise<boolean>;
   installLatestVersion(): Promise<string>;
 }
--- src/dafnyLanguageServerStartup/languageServerInstaller.ts
+++ src/dafnyLanguageServerStartup/languageServerInstaller.ts
@@ -1,165 +1,123 @@
 "use strict";
+import * as path from "path";
+import * as semver from "semver";
+import * as fs from "fs-extra";
+import * as https from "https";
+import * as os from "os";
+import * as uri from "vscode-uri";
+import { https as redirect } from "follow-redirects";
+const DecompressZip = require("decompress-zip");
+
+import { EnvironmentConfig } from "../stringRessources/_StringRessourcesModule";
+
 import { ILanguageServerInstaller } from "./ILanguageServerInstaller";
 /**
  * This is a fake implementation for the origin dafnyInstaller.ts:
  * https://github.com/DafnyVSCode/Dafny-VSCode/blob/develop/server/src/backend/dafnyInstaller.ts
+ * Some functions and code blocks were taken from the origin file.
  *
- * It simulates the online availability of the Dafny language server on a private, separate server.
+ * It simulates the online availability of the Dafny language server on our CI GitLab server.
  * In our version, the upload of the server can only be automated in a limited way and is only a temporary interim solution.
  */
 export class LanguageServerInstaller implements ILanguageServerInstaller {
-  private readonly basePath = this.resolvePath(
-    pathHelper.join(__dirname, "..", "..", "dafny")
+  private readonly tmpServerFolder: string = "dafnyTMPServer";
+
+  private readonly basePath: string = this.resolvePath(
+    path.join(__dirname, "..", "..", this.tmpServerFolder)
   );
-  private readonly downloadFile = this.resolvePath(
-    pathHelper.join(this.basePath, "..", "dafny.zip")
+  private readonly downloadFile: string = this.resolvePath(
+    path.join(this.basePath, "..", "dafny.zip")
   );
 
-  constructor(private notificationService: NotificationService) {}
+  private readonly tmpBinaryURL: string =
+    "https://gitlab.dev.ifs.hsr.ch/dafny-ba/dafny-language-server/-/jobs/180574/artifacts/download";
+  private readonly tmpReleaseVersion: string = "1.0.0";
+
+  constructor() {}
+  public anyVersionInstalled(): boolean {
+    return fs.existsSync(this.basePath); // serverExePath
+  }
+
+  public async installLatestVersion(): Promise<string> {
+    if (this.anyVersionInstalled()) {
+      this.deleteInstalledVersion();
+    }
+    this.downloadLatestServerRelease(this.tmpBinaryURL, this.downloadFile);
+
+    this.extractZip(this.downloadFile);
+    this.cleanupSetPermission();
+    return "version";
+  }
+
+  private deleteInstalledVersion(): void {
+    fs.removeSync(this.basePath);
+  }
 
   public async latestVersionInstalled(localVersion: string): Promise<boolean> {
     try {
-      const json = await this.getLatestVersion();
+      const latestVersion: string = await this.getLatestVersion();
+
       const localVersionSemVer = localVersion.match(/(\d+\.\d+\.\d+)/);
-      if (json && json.name) {
-        const latestVersion = json.name; // semver ignores leading v
-        const latestVersionSemVer = latestVersion.match(/(\d+\.\d+\.\d+)/);
-        if (localVersionSemVer != null && latestVersionSemVer != null) {
-          console.log("Local: " + localVersionSemVer[0]);
-          console.log("Remote:" + latestVersionSemVer[0]);
-          return semver.gte(localVersionSemVer[0], latestVersionSemVer[0]);
-        } else {
-          console.log("Can't parse version numbers");
-          return Promise.reject(false);
-        }
+      const latestVersionSemVer = latestVersion.match(/(\d+\.\d+\.\d+)/);
+
+      if (localVersionSemVer != null && latestVersionSemVer != null) {
+        console.log("Local: " + localVersionSemVer[0]);
+        console.log("Remote:" + latestVersionSemVer[0]);
+        return semver.gte(localVersionSemVer[0], latestVersionSemVer[0]);
       } else {
-        throw new Error("Could not read dafny version from JSON");
+        console.log("Can not parse version numbers");
+        return Promise.reject(false);
       }
     } catch (e) {
-      console.log("Can't get release information: " + e);
+      console.log("Can not get release information: " + e);
       return Promise.reject(false);
     }
   }
 
-  private getLatestVersion(): Promise<any> {
-    return new Promise<any>((resolve, reject) => {
-      const options: https.RequestOptions = {
-        headers: {
-          "User-Agent":
-            "Mozilla/5.0 (Windows NT x.y; rv:10.0) Gecko/20100101 Firefox/10.0",
-        },
-        host: uri.parse(Installer.ReleaseUrl).authority,
-        path: uri.parse(Installer.ReleaseUrl).path,
-      };
+  private resolvePath(str: string) {
+    if (str.substr(0, 2) === "~/") {
+      str =
+        (process.env.HOME ||
+          process.env.HOMEPATH ||
+          process.env.HOMEDIR ||
+          process.cwd()) + str.substr(1);
+    }
+    return path.resolve(str);
+  }
 
-      https
-        .get(options, (res) => {
-          let body = "";
-
-          res.on("data", (chunk) => {
-            body += chunk;
-          });
-
-          res.on("end", () => {
-            try {
-              const json = JSON.parse(body);
-              resolve(json);
-            } catch (e) {
-              console.log("Could not parse Dafny release information JSON");
-              reject();
-            }
-          });
-        })
-        .on("error", (e) => {
-          console.error(e);
-          return reject(e);
-        });
+  // this would be an http api request to the GitHub CI server.
+  private getLatestVersion(): Promise<string> {
+    return new Promise<string>(() => {
+      this.tmpReleaseVersion;
     });
   }
 
-  private removeCurrentVErsion() {
-    // if exists.
-  }
-
-  private downloadLatestServerRelease(json: any): Promise<boolean> {
-    if (!json || !json.assets) {
-      const msg = "Could not get Dafny Release assets from JSON response.";
-      console.log(msg);
-      return Promise.reject(msg);
-    }
-
-    let platform;
-    switch (os.platform()) {
-      case EnvironmentConfig.Win32:
-        platform = "win";
-        break;
-      case EnvironmentConfig.OSX:
-        platform = "osx";
-        break;
-      case EnvironmentConfig.Ubuntu:
-        platform = "ubuntu";
-        break;
-    }
-    if (!platform) {
-      return Promise.reject(`Unsupported platform: "${os.platform()}"`);
-    }
-
-    const url = this.getReleaseUrl(json.assets, platform);
-    if (!url) {
-      return Promise.reject(
-        `Could not find dafny release for platform "${platform}"`
-      );
-    }
-
-    return this.download(url, this.downloadFile);
-  }
-
-  private cleanupSetPermission(): Promise<string> {
-    if (os.platform() !== EnvironmentConfig.Win32) {
-      fs.chmodSync(
-        pathHelper.join(this.basePath, "dafny", "z3", "bin", "z3"),
-        "755"
-      );
-      fs.chmodSync(
-        pathHelper.join(this.basePath, "dafny", "DafnyServer.exe"),
-        "755"
-      );
-      fs.chmodSync(pathHelper.join(this.basePath, "dafny", "Dafny.exe"), "755");
-    }
-
-    fs.unlink(this.downloadFile, (err) => {
-      if (err) {
-        console.error("Error deleting archive: " + err);
-      }
-    });
-    console.log("prepared dafny");
-
-    return Promise.resolve(pathHelper.join(this.basePath, "dafny"));
-  }
-
-  private download(url: string, filePath: string): Promise<boolean> {
+  private downloadLatestServerRelease(
+    url: string,
+    filePath: string
+  ): Promise<boolean> {
     return new Promise<any>((resolve, reject) => {
       try {
-        this.notificationService.startProgress();
+        //this.notificationService.startProgress();
         const options: https.RequestOptions = {
           headers: {
             "User-Agent":
               "Mozilla/5.0 (Windows NT x.y; rv:10.0) Gecko/20100101 Firefox/10.0",
           },
-          host: uri.parse(url).authority,
-          path: uri.parse(url).path,
+          host: uri.URI.parse(url).authority,
+          path: uri.URI.parse(url).path,
         };
 
         const file = fs.createWriteStream(filePath);
         const request = redirect.get(options, (response: any) => {
           response.pipe(file);
 
-          const len = parseInt(response.headers["content-length"], 10);
-          let cur = 0;
-          response.on("data", (chunk: string) => {
-            cur += chunk.length;
-            this.notificationService.progress("Downloading Dafny ", cur, len);
-          });
+          //const len = parseInt(response.headers["content-length"], 10);
+          //let cur = 0;
+          //response.on("data", (chunk: string) => {
+          //cur += chunk.length;
+          //this.notificationService.progress("Downloading Dafny ", cur, len);
+          // });
 
           file.on("finish", () => {
             file.close();
@@ -171,27 +129,45 @@ export class LanguageServerInstaller implements ILanguageServerInstaller {
           throw err;
         });
       } catch (e) {
-        console.error("Error downloading dafny: " + e);
+        console.error("Error downloading Dafny: " + e);
         return reject(false);
       }
     });
   }
 
-  private extract(filePath: string): Promise<boolean> {
-    return new Promise<any>((resolve, reject) => {
+  private cleanupSetPermission(): Promise<string> {
+    if (os.platform() !== EnvironmentConfig.Win32) {
+      fs.chmodSync(
+        path.join(this.basePath, this.tmpServerFolder, "z3", "bin", "z3"),
+        "755"
+      );
+      fs.chmodSync(
+        path.join(this.basePath, this.tmpServerFolder, "DafnyServer.exe"),
+        "755"
+      );
+      fs.chmodSync(
+        path.join(this.basePath, this.tmpServerFolder, "Dafny.exe"),
+        "755"
+      );
+    }
+
+    fs.unlink(this.downloadFile, (err) => {
+      if (err) {
+        console.error("Error deleting archive: " + err);
+      }
+    });
+    console.log("prepared dafny");
+
+    return Promise.resolve(path.join(this.basePath, this.tmpServerFolder));
+  }
+
+  private extractZip(filePath: string): Promise<boolean> {
+    return new Promise<boolean>((resolve, reject) => {
       try {
         console.log("Extracting files...");
-        this.notificationService.startProgress();
+        //this.notificationService.startProgress();
 
         const unzipper = new DecompressZip(filePath);
-
-        unzipper.on("progress", (fileIndex: number, fileCount: number) => {
-          this.notificationService.progress(
-            "Extracting Dafny ",
-            fileIndex + 1,
-            fileCount
-          );
-        });
 
         unzipper.on("error", (e: any) => {
           if (e.code && e.code === "ENOENT") {
@@ -217,11 +193,11 @@ export class LanguageServerInstaller implements ILanguageServerInstaller {
           fs.mkdirSync(this.basePath);
         }
         unzipper.extract({
-          filter: (file: any) => file.type !== "SymbolicLink",
+          //filter: (file: any) => file.type !== "SymbolicLink",
           path: this.basePath,
         });
       } catch (e) {
-        console.error("Error extracting dafny: " + e);
+        console.error("Error extracting Dafny: " + e);
         return reject(false);
       }
     });
