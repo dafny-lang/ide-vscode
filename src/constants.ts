05b18225d30c55422f15814e77e4fe2b895d9527
Christoph Amrein <camrein@users.noreply.github.com>
2022-03-15 16:57:11 +0000

    chore: Prepare v2.3.0 (#143)
    * Prepare the release for v2.3.0
    
    * Replace v3.4.0 with v3.4.2
    
    Co-authored-by: Mikaël Mayer <MikaelMayer@users.noreply.github.com>

--- src/constants.ts
+++ src/constants.ts
@@ -45,7 +45,7 @@ export namespace DotnetConstants {
 
 export namespace LanguageServerConstants {
   export const Latest = 'latest';
-  export const LatestVersion = '3.4.0';
+  export const LatestVersion = '3.5.0';
   export const UnknownVersion = 'unknown';
   export const DownloadBaseUri = 'https://github.com/dafny-lang/dafny/releases/download';
 
