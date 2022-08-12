08b7330cb05eac27ad9a93379d8170ea36b87b57
Remy Willems <rwillems@amazon.com>
2022-08-12 22:13:55 +0200

    Correctly handle a difference between verifiable and document symbols (#249)

--- src/test/suite/verificationSymbolStatusView.test.ts
+++ src/test/suite/verificationSymbolStatusView.test.ts
@@ -32,60 +32,94 @@ module Bar {
 
 suite('Verification symbol view', () => {
   test('opening a file triggers an implicit testrun that shows stale tasks', async () => {
+    const testRunCalledPromise = toPromise(listener.createTestRunCalled.event);
+    const testRunEndPromise = toPromise(listener.testRunEndCalled.event);
+    const testItemSkipped = toPromise(listener.testRunSkippedCalled.event);
+    const untitledDocument = await vscode.workspace.openTextDocument({ content: program, language: 'dafny' });
+    const extension = vscode.extensions.getExtension('dafny-lang.ide-vscode')!;
+    await extension.activate();
+    const symbols = await vscode.commands.executeCommand('vscode.executeDocumentSymbolProvider', untitledDocument.uri) as DocumentSymbol[];
+    const foo = symbols[0];
+    const zaz = foo.children[0];
+    const m1 = zaz.children[0];
 
-    const replaceCalled: EventEmitter<TestItem[]> = new Emitter();
-    const createTestRunCalled: EventEmitter<vscode.TestRunRequest> = new Emitter();
-    const testRunEndCalled: EventEmitter<vscode.TestRun> = new Emitter();
-    const testRunSkippedCalled: EventEmitter<vscode.TestItem> = new Emitter();
-    const originalCreateTestController = vscode.tests.createTestController;
+    const replacedItems = await toPromise(listener.replaceCalled.event);
+    assert.strictEqual(replacedItems.length, 3);
+    const runRequest = await testRunCalledPromise;
+    assert.strictEqual(runRequest.include![0].id, untitledDocument.uri.toString() + JSON.stringify(m1.selectionRange));
+    const skippedItem = await testItemSkipped;
+    assert.strictEqual(skippedItem.id, untitledDocument.uri.toString() + JSON.stringify(m1.selectionRange));
+    await testRunEndPromise;
+  }).timeout(30 * 1000);
+
+  test('test runs still start and end when document symbols do not match verifiable symbols', async () => {
+
+    const originalExecuteCommand = vscode.commands.executeCommand;
+    function executeCommandMock<T = unknown>(command: string, args: any[]) {
+      if(command === 'vscode.executeDocumentSymbolProvider') {
+        return Promise.resolve<T>([] as any as T);
+      }
+      return originalExecuteCommand<T>(command, args);
+    }
+    vscode.commands.executeCommand = executeCommandMock;
+
+    try {
+      const testRunCalledPromise = toPromise(listener.createTestRunCalled.event);
+      const testRunEndPromise = toPromise(listener.testRunEndCalled.event);
+      await vscode.workspace.openTextDocument({ content: program, language: 'dafny' });
+      const extension = vscode.extensions.getExtension('dafny-lang.ide-vscode')!;
+      await extension.activate();
+
+      const replacedItems = await toPromise(listener.replaceCalled.event);
+      assert.strictEqual(replacedItems.length, 0);
+      const runRequest = await testRunCalledPromise;
+      assert.strictEqual(runRequest.include?.length, 4);
+      await testRunEndPromise;
+    } finally {
+      vscode.commands.executeCommand = originalExecuteCommand;
+    }
+  }).timeout(30 * 1000);
+});
+
+class TestControllerListener {
+
+  public readonly replaceCalled: EventEmitter<TestItem[]> = new Emitter();
+  public readonly createTestRunCalled: EventEmitter<vscode.TestRunRequest> = new Emitter();
+  public readonly testRunEndCalled: EventEmitter<vscode.TestRun> = new Emitter();
+  public readonly testRunSkippedCalled: EventEmitter<vscode.TestItem> = new Emitter();
+  private readonly originalCreateTestController = vscode.tests.createTestController;
+
+  public constructor() {
     vscode.tests.createTestController = (id: string, label: string) => {
-      const controller = originalCreateTestController(id, label);
+      const controller = this.originalCreateTestController(id, label);
       const originalReplace = controller.items.replace.bind(controller.items);
       controller.items.replace = (items: TestItem[]) => {
-        replaceCalled.fire(items);
+        this.replaceCalled.fire(items);
         originalReplace(items);
       };
 
       const originalCreateTestRun = controller.createTestRun.bind(controller);
       controller.createTestRun = (request, name, persist) => {
         const result = originalCreateTestRun(request, name, persist);
-        createTestRunCalled.fire(request);
+        this.createTestRunCalled.fire(request);
         const originalEnd = result.end.bind(result);
         result.end = () => {
-          testRunEndCalled.fire(result);
+          this.testRunEndCalled.fire(result);
           originalEnd();
         };
         const originalSkipped = result.skipped.bind(result);
         result.skipped = (item) => {
-          testRunSkippedCalled.fire(item);
+          this.testRunSkippedCalled.fire(item);
           originalSkipped(item);
         };
         return result;
       };
       return controller;
     };
+  }
 
-    try {
-      const testRunCalledPromise = toPromise(createTestRunCalled.event);
-      const testRunEndPromise = toPromise(testRunEndCalled.event);
-      const testItemSkipped = toPromise(testRunSkippedCalled.event);
-      const untitledDocument = await vscode.workspace.openTextDocument({ content: program, language: 'dafny' });
-      const extension = vscode.extensions.getExtension('dafny-lang.ide-vscode')!;
-      await extension.activate();
-      const symbols = await vscode.commands.executeCommand('vscode.executeDocumentSymbolProvider', untitledDocument.uri) as DocumentSymbol[];
-      const foo = symbols[0];
-      const zaz = foo.children[0];
-      const m1 = zaz.children[0];
-
-      const replacedItems = await toPromise(replaceCalled.event);
-      assert.strictEqual(replacedItems.length, 3);
-      const runRequest = await testRunCalledPromise;
-      assert.strictEqual(runRequest.include![0].id, JSON.stringify(m1.selectionRange));
-      const skippedItem = await testItemSkipped;
-      assert.strictEqual(skippedItem.id, JSON.stringify(m1.selectionRange));
-      await testRunEndPromise;
-    } finally {
-      vscode.tests.createTestController = originalCreateTestController;
-    }
-  }).timeout(30 * 1000);
-});
+  public dispose(): void {
+    vscode.tests.createTestController = this.originalCreateTestController;
+  }
+}
+const listener = new TestControllerListener();
