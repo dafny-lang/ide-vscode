709f1342531a55e14ae59363e9aefe86332a5356
Remy Willems <rwillems@amazon.com>
2023-01-30 22:33:07 +0100

    Add verification integration test (#348)
    * Add integration test that checks that verification through a solver works
    
    * Update test name

---
+++ src/test/verification.test.ts
@@ -0,0 +1,21 @@
+import * as assert from 'assert';
+import * as vscode from 'vscode';
+
+suite('Verification', () => {
+  test.only('Program with errors has diagnostics', async () => {
+
+    const extension = vscode.extensions.getExtension('dafny-lang.ide-vscode')!;
+    await extension.activate();
+
+    const program = 'method Foo() ensures false {}';
+    const document = await vscode.workspace.openTextDocument({ language: 'dafny', content: program });
+    await new Promise<void>(resolve => {
+      vscode.languages.onDidChangeDiagnostics(() => {
+        const diagnostics = vscode.languages.getDiagnostics(document.uri);
+        assert.equal(diagnostics.length, 1);
+        assert.equal(diagnostics[0].message.includes('postcondition'), true);
+        resolve();
+      });
+    });
+  }).timeout(3 * 60 * 1000); // We use a large timeout to allow for the Dafny installation to run.
+});
