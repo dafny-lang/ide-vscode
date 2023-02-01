09204b4aef2a032ecefee7d458e77d2e67680daf
Remy Willems <rwillems@amazon.com>
2023-02-01 18:05:52 +0100

    Remove testing code

--- src/test/verification.test.ts
+++ src/test/verification.test.ts
@@ -1,70 +1,8 @@
 import * as assert from 'assert';
 import * as vscode from 'vscode';
-import { ViewColumn } from 'vscode';
 
 suite('Verification', () => {
-  test('Program with errors has diagnostics', async () => {
-
-    const createChannel: (name: string, languageId: string | undefined) => vscode.OutputChannel = (name, language) => {
-      return {
-        name: name,
-
-        /**
-         * Append the given value to the channel.
-         *
-         * @param value A string, falsy values will not be printed.
-         */
-        append: (value: string) => {},
-
-        /**
-         * Append the given value and a line feed character
-         * to the channel.
-         *
-         * @param value A string, falsy values will be printed.
-         */
-        appendLine: (value: string) => {},
-
-        /**
-         * Replaces all output from the channel with the given value.
-         *
-         * @param value A string, falsy values will not be printed.
-         */
-        replace: (value: string) => {},
-
-        /**
-         * Removes all output from the channel.
-         */
-        clear: () => {},
-
-        /**
-         * Reveal this channel in the UI.
-         *
-         * @param preserveFocus When `true` the channel will not take focus.
-         */
-
-        /**
-         * Reveal this channel in the UI.
-         *
-         * @deprecated Use the overload with just one parameter (`show(preserveFocus?: boolean): void`).
-         *
-         * @param column This argument is **deprecated** and will be ignored.
-         * @param preserveFocus When `true` the channel will not take focus.
-         */
-        show: (column?: ViewColumn, preserveFocus?: boolean) => {},
-
-        /**
-         * Hide this channel from the UI.
-         */
-        hide: () => {},
-
-        /**
-         * Dispose and free associated resources.
-         */
-        dispose: () => {}
-      }
-    };
-    vscode.window.createOutputChannel = createChannel;
-
+  test.only('Program with errors has diagnostics', async () => {
     const extension = vscode.extensions.getExtension('dafny-lang.ide-vscode')!;
     console.log('Waiting for extension activation');
     await extension.activate();
