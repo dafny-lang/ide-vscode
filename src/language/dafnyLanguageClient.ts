dafny-lang/ide-vscode

commit a496a681c4de1909c476e0e769fec036f59fa085

Fix plugin arguments to Dafny server (#450)

src/language/dafnyLanguageClient.ts
@@ -96,7 +96,7 @@ function getMarkGhostStatementsArgument(): string {
 
 function getDafnyPluginsArgumentOld(): string[] {
   const plugins = Configuration.get<string[]>(ConfigurationConstants.LanguageServer.DafnyPlugins);
-  if(plugins === null || !Array.isArray(plugins)) {
+  if (plugins === null || !Array.isArray(plugins)) {
     return [];
   }
   return (
@@ -108,13 +108,13 @@ function getDafnyPluginsArgumentOld(): string[] {
 
 function getDafnyPluginsArgument(): string[] {
   const plugins = Configuration.get<string[]>(ConfigurationConstants.LanguageServer.DafnyPlugins);
-  if(plugins === null || !Array.isArray(plugins)) {
+  if (plugins === null || !Array.isArray(plugins)) {
     return [];
   }
   return (
     plugins
       .filter(plugin => plugin !== null && plugin !== '')
-      .map((plugin, i) => `--plugin:${i}=${plugin}`)
+      .map(plugin => `--plugin:${plugin}`)
   );
 }
 

commit c885cc9323910201bede0a44eb19b7f5a7730546

Fix formatting issues accidentally introduced in PR #450

src/language/dafnyLanguageClient.ts
@@ -96,7 +96,7 @@ function getMarkGhostStatementsArgument(): string {
 
 function getDafnyPluginsArgumentOld(): string[] {
   const plugins = Configuration.get<string[]>(ConfigurationConstants.LanguageServer.DafnyPlugins);
-  if (plugins === null || !Array.isArray(plugins)) {
+  if(plugins === null || !Array.isArray(plugins)) {
     return [];
   }
   return (
@@ -108,7 +108,7 @@ function getDafnyPluginsArgumentOld(): string[] {
 
 function getDafnyPluginsArgument(): string[] {
   const plugins = Configuration.get<string[]>(ConfigurationConstants.LanguageServer.DafnyPlugins);
-  if (plugins === null || !Array.isArray(plugins)) {
+  if(plugins === null || !Array.isArray(plugins)) {
     return [];
   }
   return (